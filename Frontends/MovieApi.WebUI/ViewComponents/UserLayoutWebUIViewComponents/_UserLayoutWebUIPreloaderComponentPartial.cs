abdfb673226441fd17c6a4c5c3c081fc30a7790c
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-10-21 12:33:32 +0300

    Login ve Register Modal Düzenlemeleri

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUIPreloaderComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+    public class _UserLayoutWebUIPreloaderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
