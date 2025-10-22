caabcac027a2751e759457c731cd66c0a92f59e1
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2025-10-22 12:03:40 +0300

    Chatgpt ile Örnek Json Dataları Oluşturma

--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
 
 namespace MovieApi.WebUI.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult MovieList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> MovieList()
         {
             ViewBag.v1 = "Film Listesi"; 
             ViewBag.v2 = "Ana Sayfa"; 
             ViewBag.v3 = "Tüm Filmler";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7220/api/Movies");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonDate = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonDate);
+                return View(values);
+            }
+
+
+
             return View();
         }
     }
